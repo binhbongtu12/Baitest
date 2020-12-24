a73abb788adc58f5cb6bfb0ef0bf2495150cd999
Long le <binhbongtu12@gmail.com>
2020-12-24 09:22:02 +0700

    Bai thu nghiem

---
+++ BaitestWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BaitestWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
